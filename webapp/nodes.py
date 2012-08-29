7e5e7aa5c0880412cff1a802736f5396562b7a0c
Jason Cannavale <jason.cannavale@rackspace.com>
2012-08-29 16:16:24 -0500

    adding node CRUD

--- webapp/nodes.py
+++ webapp/nodes.py
@@ -13,3 +13,72 @@ def node_template(htmlfile=None, extra=None, action=None, cluster_data=None, rol
                            data=extra,
                            cluster_list=cluster_data,
                            role_list=role_data) 
+
+@nodes.route('/', endpoint='index', methods=["GET"])
+def list_nodes():
+    node_url = current_app.base_url + "nodes/"
+    data = json.loads(current_app.build_request(node_url, "GET"))
+    return node_template(htmlfile=htmlfile,
+                         action='List',
+                         extra=json.loads(current_app.build_request(node_url, "GET")))
+
+@nodes.route('/create', methods=["GET", "POST"])
+def create_nodes():
+    node_url = current_app.base_url + "nodes/"
+    cluster_url = current_app.base_url + "clusters/"
+    role_url = current_app.base_url + "roles/"
+
+    cluster_data = json.loads(current_app.build_request(cluster_url, "GET"))
+    role_data = json.loads(current_app.build_request(role_url, "GET"))
+ 
+    if request.method == "GET":
+        return node_template(htmlfile=htmlfile,
+                             action='Create',
+                             cluster_data=cluster_data,
+                             role_data=role_data)
+
+    elif request.method == "POST":
+        jdata = json.dumps({"hostname": request.form['hostname'],
+                            "cluster_id": request.form['cluster'],
+                            "role_id": request.form['role']}).encode('utf-8')
+        current_app.build_request(node_url, "POST", jdata)
+        data = json.loads(build_request(node_url, "GET"))
+        return node_template(htmlfile=htmlfile,
+                             action="List",
+                             extra=data)
+
+@nodes.route('/update/<id>', methods=["GET", "POST"])
+def update_nodes(id=None):
+    node_url = current_app.base_url + "nodes/"
+    update_url = node_url + id
+
+    cluster_data = json.loads(current_app.build_request(cluster_url, "GET"))
+    role_data = json.loads(current_app.build_request(role_url, "GET"))
+
+    if request.method == "GET":
+        data = current_app.build_request(update_url, "GET")
+        return node_template(htmlfile=htmlfile,
+                             action='Upate',
+                             extra=data,
+                             cluster_data=cluster_data,
+                             role_data=role_data)
+
+    elif request.method == "POST":
+        jdata = json.dumps({"hostname": request.form['hostname'],
+                            "cluster_id": request.form['cluster'],
+                            "role_id": request.form['role']}).encode('utf-8')
+        current_app.build_request(update_url, "PUT", jdata)
+        data = json.loads(build_request(node_url, "GET"))
+        return node_template(htmlfile=htmlfile,
+                             action="List",
+                             extra=data)
+
+@nodes.route('/delete/<id>', methods=["GET"])
+def delete_nodes(id=None):
+    node_url = current_app.base_url + "nodes/"
+    delete_url = node_url + id
+    current_app.build_request(delete_url, "DELETE")
+    data = json.loads(build_request(node_url, "GET"))
+    return node_template(htmlfile=htmlfile,
+                         action="List",
+                         extra=data)
