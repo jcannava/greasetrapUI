897851608377efef3b11e7f964af70b0fe73d235
Jason Cannavale <jason.cannavale@rackspace.com>
2012-08-29 00:38:34 -0500

    Fixup/cleanup clusters.py

--- webapp/clusters.py
+++ webapp/clusters.py
@@ -2,14 +2,20 @@ from pprint import pprint
 from flask import Blueprint, Flask, session, render_template, url_for, request, current_app
 import json
 
-clusters = Blueprint('clusters', __name__)
-htmlfile = 'clusters.html'
+clusters = Blueprint('clusters', __name__, template_folder='templates', static_folder='static')
+htmlfile = 'cluster.html'
 
-@clusters.route('/', methods=['GET'])
+def cluster_template(htmlfile=None, extra=None, action=None, data=None):
+    return render_template(htmlfile,
+                           clusters=url_for('.index'),
+                           css=url_for('.static', filename='site.css'),
+                           action=action,
+                           data=extra) 
+
 def list_clusters():
-    base_url = current_app.base_url + "clusters/"
-    pprint(base_url)
-    data = json.loads(current_app.build_request(base_url), "GET")
-    return current_app.templatify(htmlfile=htmlfile,
-                                  action='List',
-                                  extra=data)
+    cluster_url = current_app.base_url + "clusters/"
+    data = json.loads(current_app.build_request(cluster_url, "GET"))
+    return cluster_template(htmlfile=htmlfile,
+                           action='List',
+                           extra=data)
+clusters.add_url_rule('/', 'index', list_clusters)
