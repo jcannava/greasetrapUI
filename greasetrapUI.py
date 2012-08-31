de6377f3fb96d05fdebe38caa6b05ab3200f51dc
Jason Cannavale <jason.cannavale@rackspace.com>
2012-08-31 14:13:14 -0500

    adding check for roush API on startup

--- greasetrapUI.py
+++ greasetrapUI.py
@@ -1,6 +1,17 @@
 from webapp import GreaseTrapUI
+import httplib2
+
+def checkRoush(url=None):
+    conn = httplib2.Http()
+    resp,content = conn.request(url, "GET") 
+    return resp.status
 
 if __name__ == '__main__':
     foo = GreaseTrapUI("greasetrapUI", configFile="greasetrapUI.conf", debug=True)
-    foo.run()
+
+    try:
+        checkRoush(foo.base_url)
+        foo.run()
+    except:
+        print "Roush API is not available. Please try again."
 
