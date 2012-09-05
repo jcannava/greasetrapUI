25eb2f299dd4c27a49302fffe9689a80b41725ff
Jason Cannavale <jason.cannavale@rackspace.com>
2012-09-05 16:07:53 -0500

    pep8 cleanup -> greasetrapUI.py

--- greasetrapUI.py
+++ greasetrapUI.py
@@ -1,17 +1,19 @@
 from webapp import GreaseTrapUI
 import httplib2
 
+
 def checkRoush(url=None):
     conn = httplib2.Http()
-    resp,content = conn.request(url, "GET") 
+    resp, content = conn.request(url, "GET")
     return resp.status
 
 if __name__ == '__main__':
-    foo = GreaseTrapUI("greasetrapUI", configFile="greasetrapUI.conf", debug=True)
+    foo = GreaseTrapUI("greasetrapUI",
+                       configFile="greasetrapUI.conf",
+                       debug=True)
 
     try:
         checkRoush(foo.base_url)
         foo.run()
     except:
         print "Roush API is not available. Please try again."
-
